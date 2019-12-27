06528c50cf4fa477c3b42be86c08f297f8fc92d6
msbonus <51191968+msbonus@users.noreply.github.com>
2019-12-27 23:12:19 +0100

    fix methods

--- swhp/ios/logger.py
+++ swhp/ios/logger.py
@@ -4,6 +4,7 @@ Logger
 
 from enum import IntEnum, auto
 
+
 class LogLevel(IntEnum):
     Non = 0
     Err = auto()
@@ -31,28 +32,32 @@ class SimpleLogger:
     def reset(cls):
         cls.ind = ""
 
-    def err(self, *msg):
-        if self.loglevel >= LogLevel.Err:
-            print(self.ind, *msg)
+    @classmethod
+    def err(cls, *msg):
+        if cls.loglevel >= LogLevel.Err:
+            print(cls.ind, *msg)
 
-    def war(self, *msg):
-        if self.loglevel >= LogLevel.War:
-            print(self.ind, *msg)
+    @classmethod
+    def war(cls, *msg):
+        if cls.loglevel >= LogLevel.War:
+            print(cls.ind, *msg)
 
     @classmethod
     def inf(cls, *msg):
         if cls.loglevel >= LogLevel.Inf:
             print(cls.ind, *msg)
 
-    def debug(self, *msg):
-        if self.loglevel >= LogLevel.Debug:
-            print(self.ind, *msg)
+    @classmethod
+    def debug(cls, *msg):
+        if cls.loglevel >= LogLevel.Debug:
+            print(cls.ind, *msg)
 
     @classmethod
     def trace(cls, *msg):
         if cls.loglevel >= LogLevel.Trace:
             print(cls.ind, *msg)
 
-    def spew(self, *msg):
-        if self.loglevel >= LogLevel.Spew:
-            print(self.ind, *msg)
+    @classmethod
+    def spew(cls, *msg):
+        if cls.loglevel >= LogLevel.Spew:
+            print(cls.ind, *msg)
